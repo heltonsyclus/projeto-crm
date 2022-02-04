2a2d92223b945b6722fd51addd8196159fde175d
HELTON SYCLUS <84809618+heltonsyclus@users.noreply.github.com>
2022-02-04 09:08:21 -0300

    teste card grupo14

--- src/commands/layouts/layoutColaborador.js
+++ src/commands/layouts/layoutColaborador.js
@@ -566,7 +566,7 @@ export function senhaLogin() {
       },
       {
         usuario: "helton",
-        senha: "0",
+        senha: "7",
         id_colaborador: 47,
         id_aplicativo_inicial: 0,
         aplicativos: [
